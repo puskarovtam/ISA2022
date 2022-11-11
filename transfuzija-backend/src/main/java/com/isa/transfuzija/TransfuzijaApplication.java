ff5bed45735d6029a0d9aed8a72cde22a36e25b5
Tamara <tamara.puskarov@gmail.com>
2022-11-11 16:08:54 +0100

    Postavljanje projekta na git

---
+++ transfuzija-backend/src/main/java/com/isa/transfuzija/TransfuzijaApplication.java
@@ -0,0 +1,13 @@
+package com.isa.transfuzija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfuzijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfuzijaApplication.class, args);
+	}
+
+}
---
+++ transfuzija-backend/src/test/java/com/isa/transfuzija/TransfuzijaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.transfuzija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfuzijaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
